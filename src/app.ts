2a971dccbf718fe2285d74c8bae8beb96f90ea79
Chandra shekhar pandey <158845488+shekharpandey10@users.noreply.github.com>
2025-10-07 20:32:01 +0530

    domain name url updated

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,8 @@ import  userRouter  from "./routes/user.routes"
 import contentRouter from "./routes/content.route"
 import cors from 'cors'
 
-const domains=["brainly-front-end-rtjd.vercel.app","http://localhost:5173"]
+const domains=[ "http://localhost:5173",
+  "https://www.brainly-front-end-rtjd.vercel.app","http://localhost:5173"]
 const app=express()
 app.use(cors({
   origin: domains,
